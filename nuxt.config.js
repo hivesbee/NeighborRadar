331f4cb43d10c9b6366f53df82b64eadb692bb1f
hivesbee <hivesbee@gmail.com>
2018-03-25 05:44:21 +0900

    favicon 設定追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,17 @@
-// file: nuxt.config.js
 module.exports = {
+  // ヘッダ
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' },
+      { rel: 'icon', href:'favicon.ico' }
+    ]
+  },
+  // モジュール
   modules: [ '@nuxtjs/vuetify' ],
+  // プラグイン
   plugins: [ '~/plugins/vue2-google-maps' ]
 }
