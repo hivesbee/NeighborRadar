0b610e92aeba0ca88d2be6d830e0343a8abd9bea
hivesbee <hivesbee@gmail.com>
2018-03-30 02:15:35 +0900

    key を env からのみ取得するように変更

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
-// import config from '../google-map.config'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.GOOGLE_MAP_KEY || config.key,
+    key: process.env.GOOGLE_MAP_KEY,
     libraries: ['places', 'geocoder'],
     options: {
       streetViewControl: false,
