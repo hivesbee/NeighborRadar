c83e76fcf05fcd376124c33fdfa90972f621857c
hivesbee <hivesbee@gmail.com>
2018-03-30 02:51:10 +0900

    config から取得するように変更

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
+import config from '../google-map.config'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.GOOGLE_MAP_KEY,
+    key: config.key,
     libraries: ['places', 'geocoder'],
     options: {
       streetViewControl: false,
