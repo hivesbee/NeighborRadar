02a4c9716daa509bc231d871d7fd310f07b657bc
hivesbee <hivesbee@gmail.com>
2018-04-07 00:43:43 +0900

    WIP component 化

---
+++ components/NinjaAdmax.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- admax -->
+    <script src="//adm.shinobi.jp/s/271d97f7450ac288f8168ef1edc6297f"></script>
+    <!-- admax -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ninja-admax"
+  }
+</script>
+
+<style scoped>
+  div {
+    /*width: 300px;*/
+    /*height: 250px;*/
+    /*background: blue;*/
+  }
+</style>
